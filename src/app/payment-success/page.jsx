2a28ec3198ba85bab2f04ad4d65496b95aab1fae
TeeshaGuptaNGTS <ngtsteesha@gmail.com>
2025-11-03 15:13:10 +0530

    update

--- src/app/payment-success/page.jsx
+++ src/app/payment-success/page.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+// import React from 'react'
+// import PaymentSuccess from './_component/successpage'
+
+// export default function Payment() {
+//   return (
+//     <PaymentSuccess/>
+//      )
+// }
+
+import React, { Suspense } from 'react'
 import PaymentSuccess from './_component/successpage'
 
 export default function Payment() {
   return (
-    <PaymentSuccess/>
+    <Suspense fallback={
+      <div className="min-h-screen flex justify-center items-center text-gray-600">
+        Loading payment details...
+      </div>
+    }>
+       <PaymentSuccess/>
+      {/* <PaymentSuccessContent name={name} amount={amount} orderId={orderId} /> */}
+    </Suspense>
+   
      )
 }
